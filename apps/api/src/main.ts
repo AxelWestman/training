aec0a81dbfb27a874673b1c1d6e75a94531bb245
Axel W Notebook <axelwestman@outlook.com>
2026-07-04 13:19:20 -0300

    feat: agregado de conexión con la bd

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.enableShutdownHooks();
+
   const port = process.env.API_PORT ?? 3001;
   await app.listen(port);
   console.log(`API running on http://localhost:${port}`);
